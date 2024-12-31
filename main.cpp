04618d56c4c78d51592e0729a82319876a5af8f8
FrozenDrake <sethlipav@gmail.com>
2024-12-31 10:02:42 -0800

    added solution

--- main.cpp
+++ main.cpp
@@ -5,68 +5,29 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <climits>
+#include <map>
+#include <set>
+#include <stdexcept>
 
 using namespace std;
 
 const string STATION_HEADER = "[Stations]";
 const string AVAILABILITY_REPORT_HEADER = "[Charger Availability Reports]";
 
+
 /**
  * =============
- * Charger
+ * errorStation
  * =============
- * - manages the uptime for a single charger
- * - uses given start and end time of each report to calculate the uptime
+ * custom error class
 */
-class Charger {
-    private:
-        int chargerID;
-
-        long upTime;
-        long downTime;
-
-        long lastKnownTime;
+class errorStation : public runtime_error {
 
     public:
-        Charger(int chargerID) {
-            this->chargerID = chargerID;
+    errorStation(const string& errorStationMessage = "") : runtime_error(errorStationMessage) {
 
-            upTime = 0;
-            downTime = 0;
-
-            // -1 indicates uninitialized
-            lastKnownTime = -1;
-        }
-
-        void addTime(long start, long end, bool up) {
-            //add to the downtime if the was a gap of unrecorded time in the log file
-            if (lastKnownTime < 0) {
-                lastKnownTime = start;
-            }
-            downTime += (start - lastKnownTime);
-            lastKnownTime = end;
-
-            long time = end - start;
-
-            //add the time respectively
-            if (up) {
-                upTime += time;
-            } else {
-                downTime += time;
-            }
-        }
-
-        int getChargerID() {
-            return chargerID;
-        }
-
-        // calculate what % of the time the charger was up by dividing the uptime by the total time
-        int getPercentUptime() {
-            float percent =  ((float) upTime/ (float) (upTime + downTime));
-            percent *= 100.0;
-            return (int) percent;
-        }
-
+    }
 };
 
 /**
@@ -75,48 +36,216 @@ class Charger {
  * =============
  * - manages the uptime for a single station
  * - calculates the uptime for a station
- * - takes the average of all its chargers
+ * 
 */
 class Station {
     private:
-        vector<Charger> chargers;
         int stationID;
+
+        long minTime;
+        long maxTime;
+
+        bool empty;
+
+        vector<pair<long, long>*> timeSequences;
+
+        // check if the sequence at index j overlaps with other sequences after it
+        void nextCheck(int j) {
+            for (int k = j + 1; k < timeSequences.size(); k++) {
+                if (timeSequences[j]->second >= timeSequences[k]->second) {
+                    timeSequences.erase(timeSequences.begin() + k);
+                }
+                if (timeSequences[j]->second >= timeSequences[k]->first) {
+                    timeSequences[j]->second = timeSequences[k]->second;
+                    timeSequences.erase(timeSequences.begin() + k);
+                    break;
+                }
+            }
+        }
+
+        // check if the sequence at index j overlaps with other sequences before it
+        void prevCheck(int j) {
+            for (int k = j - 1; k > -1; k--) {
+                if (timeSequences[j]->first <= timeSequences[k]->first) {
+                    timeSequences.erase(timeSequences.begin() + k);
+                }
+                if (timeSequences[j]->first < timeSequences[k]->second) {
+                    timeSequences[j]->first = timeSequences[k]->first;
+                    timeSequences.erase(timeSequences.begin() + k);
+                    break;
+                }
+            }
+        }
+
+        // check if a time sequence is within the time sequence at index j
+        int underlapCheck(int j, pair<long,long> *timeSeq) {
+            if (timeSeq->first >= timeSequences[j]->first && timeSeq->second <= timeSequences[j]->second) {
+                // do not need to insert this sequence as it is redundant
+                return 1;
+            } 
+            return 0;
+        }
+
+        // check if a time sequence completely overlaps the one at index j in timeSequences
+        int supercedeCheck(int j, pair<long,long> *timeSeq) {
+            if (timeSeq->first < timeSequences[j]->first && timeSeq->second > timeSequences[j]->second) {
+                timeSequences[j]->second = timeSeq->second;
+                timeSequences[j]->first = timeSeq->first;
+
+                // check if the sequence keeps overlapping with the next sequences
+                nextCheck(j);
+                // check if the sequence keeps overlapping with the previous sequences
+                prevCheck(j);
+                return 1;
+            }
+            return 0;
+        }
+
+        // check if a time sequence overlaps the right side of the one at index j in timeSequences
+        int overlapGreaterCheck(int j, pair<long,long> *timeSeq) {
+            if (timeSeq->first <= timeSequences[j]->second && timeSeq->second > timeSequences[j]->second) {
+                // combine the sequences due to overlap
+                timeSequences[j]->second = timeSeq->second;
+                
+                // check if the new combined sequence overlaps with the next sequences
+                nextCheck(j);            
+                return 1;
+            }
+            return 0;
+        }
+
+         // check if a time sequence overlaps the left side of the one at index i in timeSequences
+        int overlapLessCheck(int j, pair<long,long> *timeSeq) {
+            if (timeSeq->second >= timeSequences[j]->first && timeSeq->first < timeSequences[j]->first) {
+                // combine the sequences due to overlap
+                timeSequences[j]->first = timeSeq->first;
+                
+                // check if the new combined sequence overlaps with the previous sequences
+                prevCheck(j);
+                return 1;
+            }
+            return 0;
+        }
+
+         // check if a time sequence fits inbetween the sequences at index i and i - 1 in timeSequences 
+        int betweenCheck(int j, pair<long,long> *timeSeq) {
+            if ((j - 1 > -1 && (timeSeq->second < timeSequences[j]->first && timeSeq->first > timeSequences[j - 1]->second)) || timeSeq->second < timeSequences[j]->first) {
+                timeSequences.insert(timeSequences.begin() + j, timeSeq);
+                return 1;
+            }
+            return 0;
+        }
 
     public:
         Station(int id) {
             stationID = id;
+
+            // set the min and max time to flagged values
+            minTime = LONG_MAX;
+            maxTime = -1;
+
+            timeSequences = vector<pair<long,long>*>();
+            
         }
 
-        void addNewCharger(int chargerID) {
-            chargers.push_back(Charger(chargerID));
+        ~Station() {
+            for (const auto& timeSeq : timeSequences) {
+                delete timeSeq;
+            }
+            timeSequences.clear();
+        }
+
+        bool operator!=(const Station station) const {
+            return stationID != station.stationID;
+        }
+
+        bool operator<(const Station station) const {
+            return stationID < station.stationID;
+        }
+
+        bool operator>(const Station station) const {
+            return stationID > station.stationID;
         }
 
         // find the correct charger then add time to it
-        void addTimeToCharger(int chargerID, long start, long end, bool up) {
-            for(int i = 0; i < chargers.size(); i++) {
-                if (chargers[i].getChargerID() == chargerID) {
-                    // cout << "found charger " << chargerID << "\n";
-                    chargers[i].addTime(start, end, up);
+        void addTime(long start, long end, bool up) {
+            // cout << "size " << timeSequences.size() << "\n";
+            if (end < start) {
+                return;
+            }
+            
+            // cout << "adding time to station " << stationID << "\n";
+            // cout << "start " << start << " end " << end << "\n";
+            // cout << "curMin " << minTime << " curMax " << maxTime << "\n";
+            if (start < minTime) {
+                minTime = start;
+            }
+            if (end > maxTime) {
+                maxTime = end;
+            }
+            // cout << "curMin " << minTime << " curMax " << maxTime << "\n";
 
+            // only need to keep track of sequences where chargers were available
+            if (up) {
+                // unique_ptr<pair<long,long>> timeSeq = make_unique<pair<long, long>>(start, end);
+                pair<long,long> *timeSeq = new pair<long, long>(start, end);
+
+                // if the vector of sequences is empty just insert the new sequence
+                if (timeSequences.empty()) {
+                    // cout << "there were no time sequences before\n";
+                    timeSequences.reserve(1);
+                    timeSequences.push_back(timeSeq);
                     return;
                 }
-            }
+                
+                // cout << "vector [0] " << timeSequences[0] << "\n";
+ 
+                // compare the new time sequence against other sequences to check if they can be combined
+                // or insert the new sequence into the vector if it does not overlap
+                int j;
+                for(j = 0; j < timeSequences.size(); j++) {
+                    // cout << "size " << timeSequences.size() << "\n";
+                    // cout << "comparing " << timeSeq.first << "," << timeSeq.second << " to " << timeSequences[j].first << "," << timeSequences[j].second << "\n";
 
+                    // check if the new sequence underlaps the current one
+                    if (underlapCheck(j, timeSeq)) return;
+                    // check if the new sequence supercedes the current one
+                    if (supercedeCheck(j, timeSeq)) return;
+                    // check if the new sequence overlaps with the current sequence's upper bound
+                    if (overlapGreaterCheck(j, timeSeq)) return;
+                    // check if the new sequence overlaps with the current sequence's lower bound
+                    if (overlapLessCheck(j, timeSeq)) return;
+                    // check if the new sequence fits inbetween the two sequences around it
+                    if (betweenCheck(j, timeSeq)) return;
+                }
+                // the time sequence was greater than all other ones so put it at the back
+                timeSequences.push_back(timeSeq);
+                return;
+            }
         }
 
         int getStationID() {
             return stationID;
         }
 
-        // calculate the uptime % of the station by finding the average uptime % of all its chargers
+        // calculate the uptime % of the station by summing the sequences and dividing it by the total time it was in the log
         int getPercentUptime() {
+            // cout << "calculating uptime for station " << stationID << "\n";
+            long double totalTime = maxTime - minTime;
 
-            int sum = 0;
-            for(int i = 0; i < chargers.size(); i++) {
-                sum += chargers[i].getPercentUptime();
+            long double upTime = 0;
+            for(int i = 0; i < timeSequences.size(); i++) {
+                // cout << "sequence " << i << " " << timeSequences[i]->first << "," << timeSequences[i]->second << "\n";
+                upTime += timeSequences[i]->second - timeSequences[i]->first;
             }
 
-            return sum / chargers.size();
+            // cout << "totalTime: " << totalTime << " upTime " << upTime << "\n";
+
+            return (int) ((upTime / totalTime) * 100.0);
+        }
+
+        void setStationID(int id) {
+            stationID = id;
         }
 };
 
@@ -130,7 +259,7 @@ class Station {
 */
 class LogFileProcesser {
     private:
-        vector<Station> stations;
+        map<int,Station*> stations;
         string currentLogLine = "";
         ifstream logFile;
 
@@ -156,26 +285,29 @@ class LogFileProcesser {
                 } else {
                     stringstream ss(currentLogLine);
                     string token;
-                    bool newStation = true;
+                    int stationID = -1;
+                    Station *newStation = new Station(stationID);
 
                     while(getline(ss,token, ' ')) {
                         int id;
+                        
                         try {
                             id = stoi(token);
                         } catch (exception e) {
                             // there was not a valid id
+                            throw errorStation("invalid format of station or charger id\n");
                             return 0;
                         }
                         
-                        if (newStation) {
+                        if (stationID < 0) {
                             // only the first token is the station
-                            stations.push_back(Station(id));
-                            newStation = false;
+                            stationID = id; 
+                            // cout << "making new station with id " << id << "\n";
+                            newStation->setStationID(stationID);
                         } else {
                             // otherwise it is a charger within that station
-                            // because "push_back()" is being used the newest station
-                            // is at the back of the list
-                            stations.back().addNewCharger(id);
+                            stations[id] = newStation;
+                            // cout << "charger " << id << " assigned to station " << newStation << "\n";
                         }
                     }
                 }
@@ -187,10 +319,12 @@ class LogFileProcesser {
         // parse the second section of the log file
         // to track the uptime of each station and charger
         int processAvailabilityReports() {
+            // cout << "processing reports\n";
             // get to the header for all the reports
             getToHeader(AVAILABILITY_REPORT_HEADER);
             
             while(getline(logFile,currentLogLine)) {
+                // cout << "processing " << currentLogLine << "\n";
                 // check for the end of this section
                 if (currentLogLine.compare("") == 0) {
                     break;
@@ -212,11 +346,12 @@ class LogFileProcesser {
                         long end = stol(tokens[2]);
                         bool up = tokens[3].compare("true") == 0 ? true : false;
 
-                        for (int i = 0; i < stations.size();i++) {
-                            stations[i].addTimeToCharger(id, start, end, up);
-                        }
+                        // cout << "adding time to station " << stations[id] << "\n";
+                        stations[id]->addTime(start,end,up);
+                        // cout << "added time\n";
                     } catch (exception e) {
                         // one or more of the values was not in the expected format
+                        throw errorStation("invalid format of charger availability report\n");
                         return 0;  
                     }
                 }
@@ -227,6 +362,14 @@ class LogFileProcesser {
         }
 
     public:
+
+        ~LogFileProcesser() {
+            for (const auto& pair : stations) {
+                delete pair.second;
+            }
+            stations.clear();
+        }
+
         // attempts to read the file and parse it
         string processFile(string filePath) {
             
@@ -246,10 +389,28 @@ class LogFileProcesser {
                 return "ERROR\n";
             }
 
+            set<int> uniqueStations;
+
             // make a string in the desired format
             string uptimeReport = "";
-            for (int i = 0; i < stations.size();i++) {
-                uptimeReport += to_string(stations[i].getStationID()) + " " + to_string(stations[i].getPercentUptime()) + "\n";
+            for (const auto& pair : stations) {   
+                // cout << "getting uptime for station: " << stations[i].getStationID() << "\n";
+                int id = pair.second->getStationID();
+                // cout << "looking at charger " << pair.first << " associated with station " << id <<"\n";
+                if(uniqueStations.find(id) == uniqueStations.end()) {
+                    int uptime = pair.second->getPercentUptime();
+                    // cout << "uptime of station " << uptime << "\n";
+                    uptimeReport += to_string(id) + " " + to_string(uptime) + "\n";
+                }
+                
+                uniqueStations.insert(pair.second->getStationID());
+
+                // cout << "unique Stations ";
+                // for (const auto& elem : uniqueStations) {
+                //     cout << elem << " ";
+                // }
+                // cout << "\n";
+                
             }
 
             return uptimeReport;
@@ -269,42 +430,19 @@ class LogFileProcesser {
 */
 class UnitTest {
     public:
-        // test to ensure the charger calculate the correct percent of uptime
-        static bool test1() {
-            // define the chargers
-            Charger charger1 = Charger(1);
-            Charger charger2 = Charger(2);
-
-            charger1.addTime(60000,127823,true);
-            charger2.addTime(0,1878,false);
-
-            return charger1.getPercentUptime() == 100 && charger2.getPercentUptime() == 0;
-        }
 
         // test to ensure the stations calculate the correct percent of uptime
-        static bool test2() {
+        static bool test1() {
             // define the chargers and stations
             Station station1 = Station(1);
             Station station2 = Station(2);
 
-            Charger charger1 = Charger(1);
-            Charger charger2 = Charger(2);
-            Charger charger3 = Charger(3);
-            Charger charger4 = Charger(4);     
-
-            // add the chargers to the stations
-            station1.addNewCharger(1);
-            station1.addNewCharger(2);
-
-            station2.addNewCharger(3);
-            station2.addNewCharger(4);
-
             // add time to the chargers
-            station1.addTimeToCharger(1,25000,50000,true);
-            station1.addTimeToCharger(2,27000,90900,true);
+            station1.addTime(25000,50000,true);
+            station1.addTime(27000,90900,true);
 
-            station2.addTimeToCharger(3,25000,50000,false);
-            station2.addTimeToCharger(4,27000,90900,false);
+            station2.addTime(25000,50000,false);
+            station2.addTime(27000,90900,false);
 
             // check if the calculated time is correct
             return station1.getPercentUptime() == 100 && station2.getPercentUptime() == 0;
@@ -316,18 +454,18 @@ int main(int argc, char* argv[]) {
         cout << "ERROR";
         return 1;
     }
-
+    
     LogFileProcesser processor;
 
     string uptimeReport = processor.processFile(argv[1]); 
     cout << uptimeReport;
+    
 
     /* unit tests for charger and station classes
 
     bool test1 = UnitTest::test1();
-    bool test2 = UnitTest::test2();
 
-    cout << test1 << " " << test2;
+    cout << "test1 results: " << test1 << "\n";
     */
 
     return 0;
